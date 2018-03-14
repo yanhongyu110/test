0b08235ffbd156c46cbe7a8de5e3bb4c3b10c00e
yanhongyu110 <517444425@qq.com>
2018-03-14 10:44:57 +0800

    switch demo1

--- src/test/DemoGit.java
+++ src/test/DemoGit.java
@@ -4,4 +4,8 @@ public class DemoGit {
 	public static void main(String[] args) {
 		System.out.println("this is a demo...");
 	}
+	
+	public void switch1() {
+		System.out.println("this is a switchDemo");
+	}
 }
